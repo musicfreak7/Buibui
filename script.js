8cfa71368a1d179e3b8e202543897539fd79a5b2
musicfreak7 <38415194+musicfreak7@users.noreply.github.com>
2024-02-09 01:52:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ var confetti = new ConfettiGenerator(confettiSettings);
 confetti.render();
 
  let p = document.createElement("p");
- p.innerText = "Congrats you made the right choice 🎉 \nDM me the time and venue 😉";
+ p.innerText = "Good! Chaku fek ke maarta nhi toh 😤 I Loub You💚💖❤️😘";
   p.style.fontSize = "2rem";
   p.style.fontWeight = "bold";
   p.style.textAlign = "center";
